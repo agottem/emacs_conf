d5b96d61ac890a489b70f627fa0193cfcd45711a
Andrew Gottemoller <agottem@rd-ws.resdev.cc>
2025-12-01 15:25:36 -0600

    1.  Add elpy options

--- lib/packages.el
+++ lib/packages.el
@@ -17,6 +17,10 @@
     :init
     (elpy-enable))
 
+(setq elpy-rpc-python-command "python3")
+(setq python-shell-interpreter "python3")
+(setq python-shell-interpreter-args "-i")
+
 (use-package csv-mode
     :ensure t)
 
