e4b088829405e5cfaa0111b3a35f358ef470357d
Andrew Gottemoller <andrew@resdev.tech>
2026-08-31 16:05:16 -0500

    Better indentation rules for treesitter, font size selection by host

--- lib/display.el
+++ lib/display.el
@@ -1,3 +1,11 @@
 ;; -*- lexical-binding: t; -*-
 
 (setq doom-theme 'doom-city-lights)
+
+(let ((height
+          (pcase system-name
+              ("rd-udev.resdev.tech" 90)
+              ("rd-ws.resdev.tech"   100)
+              (_                     100))))
+    (set-face-attribute 'default nil :height height)
+    (add-to-list 'default-frame-alist `(font . ,(format "DejaVu Sans Mono-%d" (/ height 10)))))
--- lib/styles.el
+++ lib/styles.el
@@ -3,4 +3,11 @@
 (defun styles-c-mode ()
     (setq c-ts-indent-offset            4)
     (setq c-ts-common-list-indent-style 'align)
-    (c-ts-mode-set-style                'bsd))
+    (c-ts-mode-set-style                'bsd)
+
+    ;; indent rules for function prototypes and ifndef preproc
+    (setf (alist-get 'c treesit-simple-indent-rules)
+        (append
+            '(((node-is "function_declarator") parent-bol 0)
+                 ((parent-is "preproc_\\(if\\|ifdef\\|elif\\|else\\)") parent-bol 0))
+            (alist-get 'c treesit-simple-indent-rules))))
